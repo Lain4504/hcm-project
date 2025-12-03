80ca82b4da56d33b6d742070078c07c14b7c2bbf
Lain <134944634+Lain4504@users.noreply.github.com>
2025-12-03 10:43:26 +0700

    fix server side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // Enable server-side rendering for API routes
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
